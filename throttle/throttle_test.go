bc66902cda741413601269262ade90360537467f
Chip Overclock <coverclock@diag.com>
2018-09-06 12:43:19 -0600

    [1] Phase 2 of throttle unit test passes.

--- throttle/throttle_test.go
+++ throttle/throttle_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/coverclock/com-diag-vamoose/ticks"
 )
 
-func TestThrottle(t * testing.T) {
+func TestThrottle1(t * testing.T) {
 	var increment ticks.Ticks = 100
 	var limit ticks.Ticks = 10
 	var now ticks.Ticks = 0
@@ -307,6 +307,315 @@ func TestThrottle(t * testing.T) {
     now = now + increment
     if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
     if (!that.Commit()) {} else { t.Error("FAILED!") }
+    
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+ 
+    if (that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (!that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+
+}
+
+func TestThrottle10(t * testing.T) {
+	var increment ticks.Ticks = 100
+	var limit ticks.Ticks = 10
+	var now ticks.Ticks = 0
+	var size Events = 10;
+	var stuff string = ""
+	
+	// CONSTRUCTORS
+
+	that := New(increment, limit, ticks.Ticks(0))
+    
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (!that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // SUSTAINED
+    
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))  
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))  
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size)) 
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+   
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+ 
+    if (that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (!that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // BURST
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+      
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) - 1;
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+     
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+ 
+    if (!that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (!that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // FILL
+    
+    now = now + (increment * ticks.Ticks(size)) - 2
+    if (that.Request(now) == 2) {} else { t.Error("FAILED!") }
+    if (that.Commits(size)) {} else { t.Error("FAILED!") }
+     
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (!that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (that.IsFull()) {} else { t.Error("FAILED!") }
+    if (that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (that.Filled()) {} else { t.Error("FAILED!") }
+    if (that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // RECOVER
+    
+    now = now + (increment * ticks.Ticks(size)) + 1
+    if (that.Request(now) == 1) {} else { t.Error("FAILED!") }
+    if (that.Commits(size)) {} else { t.Error("FAILED!") }
+     
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (!that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (that.IsFull()) {} else { t.Error("FAILED!") }
+    if (that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    now = now + (increment * ticks.Ticks(size)) + 1
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+     
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (!that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // REQUEST, RE-REQUEST, COMMIT
+    
+    now = now + (increment * ticks.Ticks(size)) - 2
+    if (that.Request(now) == 2) {} else { t.Error("FAILED!") }
+    
+    now = now + 1
+    if (that.Request(now) == 1) {} else { t.Error("FAILED!") }
+      
+    now = now + 1
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+ 
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (!that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // REQUEST, DELAY, ADMIT
+    
+    now = now + (increment * ticks.Ticks(size)) - 2
+    if (that.Request(now) == 2) {} else { t.Error("FAILED!") }
+    
+    now = now + 2 
+    if (!that.Admits(now, size)) {} else { t.Error("FAILED!") }
+ 
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (!that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (!that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // UPDATE
+    
+    now = now + (increment * ticks.Ticks(size)) + 10
+    if (!that.Update(now)) {} else { t.Error("FAILED!") }
+    
+    stuff = that.String()
+    if (len(stuff) > 0) {} else { t.Error("FAILED!") }
+    t.Log(stuff)
+
+    if (that.IsEmpty()) {} else { t.Error("FAILED!") }
+    if (!that.IsFull()) {} else { t.Error("FAILED!") }
+    if (!that.IsAlarmed()) {} else { t.Error("FAILED!") }
+    if (that.Emptied()) {} else { t.Error("FAILED!") }
+    if (!that.Filled()) {} else { t.Error("FAILED!") }
+    if (!that.Alarmed()) {} else { t.Error("FAILED!") }
+    if (that.Cleared()) {} else { t.Error("FAILED!") }
+    
+    // SUSTAINED AGAIN
+        
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))  
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))  
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size)) 
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+   
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
+
+    now = now + (increment * ticks.Ticks(size))
+    if (that.Request(now) == 0) {} else { t.Error("FAILED!") }
+    if (!that.Commits(size)) {} else { t.Error("FAILED!") }
     
     stuff = that.String()
     if (len(stuff) > 0) {} else { t.Error("FAILED!") }
--- ticks/ticks_test.go
+++ ticks/ticks_test.go
@@ -4,15 +4,21 @@ import (
     "testing"
 )
 
-func TestTicks(t * testing.T) {
+func TestFREQUENCY(t * testing.T) {
     if (FREQUENCY == 1000000000) {} else { t.Error("FAILED!") }
+}
 
-    if (Frequency() == 1000000000) {} else { t.Error("FAILED!") }	
+func TestFrequency(t * testing.T) {
+   if (Frequency() == 1000000000) {} else { t.Error("FAILED!") }	
+}
 
+func TestNow(t * testing.T) {
 	before := Now()
+	t.Logf("before=%dns\n", before);
 	if (before > 0) {} else { t.Error("FAILED!") }
-
 	after := Now()
+	t.Logf("after=%dns\n", after);
 	if (after > 0) {} else { t.Error("FAILED!") }
+	t.Logf("delta=%dns\n", after - before);
 	if (after > before) {} else { t.Error("FAILED!") }
 }
